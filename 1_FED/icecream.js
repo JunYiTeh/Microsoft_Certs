5bad2677d29c4a8793f6a9a0c128866ce814efd1
Jun Yi Teh <junyiteh2000@gmail.com>
2023-04-05 22:52:23 +0930

    First commit

---
+++ 1_FED/icecream.js
@@ -0,0 +1,38 @@
+// let iceCreamFlavours = [
+//     "Chocolate",
+//     "Strawberry",
+//     "Vanilla",
+//     "Pistachio",
+//     "Neopolitan",
+//     "Mint Chip",
+//     "Raspberry"
+// ];
+
+let iceCreamFlavours = [
+    {name: "Chocolate", type: "Chocolate", price: 2},
+    {name: "Strawberry", type: "Fruit", price: 1},
+    {name: "Vanilla", type: "Vanilla", price: 2},
+    {name: "Pistachio", type: "Nuts", price: 1.5},
+    {name: "Neopolitan", type: "Chocolate", price: 2},
+    {name: "Mint Chip", type: "Chocolate", price: 1.5},
+    {name: "Raspberry", type: "Fruit", price: 1},
+]
+
+// {scoops: [], total: }
+let transactions = []
+transactions.push({scoops: ["Chocolate", "Vanilla", "Mint Chip"], total: 5.5})
+transactions.push({scoops: ["Raspberry", "Strawberry"], total: 2})
+transactions.push({scoops: ["Vanilla", "Vanilla"], total: 4})
+
+const total = transactions.reduce((acc, curr) => acc + curr.total, 0);
+console.log(`You've made $${total} today!`)
+
+let flavourDistribution = transcations.reduce((acc, curr) => {
+    curr.scoops.forEach(scoop => {
+        if (!acc[scoop]) {
+            acc[scoop] = 0;
+        }
+        acc[scoop]++;
+    })
+    return acc;
+})
